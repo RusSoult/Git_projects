2ec358d893afc05da5042f7f841b9c7d9fa943f4
rus2 <topvoin@mail.ru>
2024-03-04 18:00:22 +0500

    add + func

--- func.py
+++ func.py
@@ -2,8 +2,8 @@ def sum(x, y):
     print(x + y)
 sum(5,6)
 
-#
-# def deduct(a, b):
-#     print(a - b)
-# deduct(10,4)
+
+def deduct(a, b):
+    print(a - b)
+deduct(10,4)
 
---
+++ main.py
@@ -0,0 +1,5 @@
+from func import sum
+from func import deduct
+
+print(sum)
+print(deduct)
